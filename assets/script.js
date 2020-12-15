256043285d8eb1153b60c928d1116be19dd863ef
Samuel Koch <skoch@ccmsi.com>
2020-12-15 18:27:13 -0500

    added variable..

--- assets/script.js
+++ assets/script.js
@@ -71,12 +71,15 @@ $(function() {
         var s = frequencyOutput.val();
         var c = s.split("");
         var itemCount= {};
+        var options= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
         for(var i=0; i< c.length; i++)
         {
             if (itemCount[c[i]] == undefined)
                     itemCount[c[i]] = 0;
                 itemCount[c[i]]++;
+
+                 
         }
         var finalDisplay = '';
         for (var i in itemCount)
